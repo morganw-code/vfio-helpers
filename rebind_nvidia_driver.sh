528184fe3b7fe6795f1addcf248ee2fde92f4408
Morgan Webb <mnbwebb2233@gmail.com>
2020-06-07 10:05:11 +1000

    Refactor

--- rebind_nvidia_driver.sh
+++ rebind_nvidia_driver.sh
@@ -1,20 +1,15 @@
-echo -n 0000:09:00.0 > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind 0000:09:00.0"
-echo -n 0000:09:00.1 > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind 0000:09:00.1"
-echo -n 0000:09:00.2 > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind 0000:09:00.2"
-echo -n 0000:09:00.3 > /sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind 0000:09:00.3"
+#!/bin/bash
+declare -A drivers
+drivers=(['0000:09:00.0']='nvidia' ['0000:09:00.1']='snd_hda_intel' ['0000:09:00.2']='xhci_hcd' ['0000:09:00.3']='nvidia-gpu')
+for device in '0000:09:00.0' '0000:09:00.1' '0000:09:00.2' '0000:09:00.3'; do
+    path="/sys/bus/pci/drivers/${drivers[$device]}/bind"
+    echo -n $device >/sys/bus/pci/drivers/vfio-pci/unbind || echo "Failed to unbind $device"
+    echo -n $device >$path || echo "Failed to bind $device at $path"
+done
 
-echo -n 1022 1482 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
-echo -n 1022 1483 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
-echo -n 10de 1e81 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
-echo -n 10de 10f8 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
-echo -n 10de 1ad8 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
-echo -n 10de 1ad9 > /sys/bus/pci/drivers/vfio-pci/remove_id || echo "id no bueno"
+for id in '1022 1482' '1022 1483' '10de 1e81' '10de 10f8' '10de 1ad8' '10de 1ad9'; do
+    echo -n $id >/sys/bus/pci/drivers/vfio-pci/remove_id || echo "Id '$id' is no bueno"
+done
 
 modprobe -r vfio-pci
-
-echo -n 0000:09:00.0 > /sys/bus/pci/drivers/nvidia/bind || echo "Failed to bind 0000:09:00.0"
-echo -n 0000:09:00.1 > /sys/bus/pci/drivers/snd_hda_intel/bind || echo "Failed to bind 0000:09:00.1"
-echo -n 0000:09:00.2 > /sys/bus/pci/drivers/xhci_hcd/bind || echo "Failed to bind 0000:09:00.2"
-echo -n 0000:09:00.3 > /sys/bus/pci/drivers/nvidia-gpu/bind || echo "Failed to bind 0000:09:00.3"
-
 systemctl restart sddm
